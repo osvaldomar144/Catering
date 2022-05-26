d902d2c79eb6360cfd8888eeca857ac73c45b2ae
Alexis <osvaldomar144@gmail.com>
2022-05-27 00:29:26 +0200

    Added functions for addChef and delete buffet from List
    (methods on ChefController)
    (added views for chef and buffet)

--- catering/src/main/java/com/catering/demo/controller/ChefController.java
+++ catering/src/main/java/com/catering/demo/controller/ChefController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.catering.demo.controller.validator.ChefValidator;
+import com.catering.demo.model.Buffet;
 import com.catering.demo.model.Chef;
+import com.catering.demo.service.BuffetService;
 import com.catering.demo.service.ChefService;
 
 @Controller
@@ -25,6 +27,9 @@ public class ChefController {
 
 	@Autowired
 	private ChefService chefService;
+	
+	@Autowired
+	private BuffetService buffetService;
 	
 	@Autowired
 	private ChefValidator chefValidator;
@@ -45,12 +50,30 @@ public class ChefController {
 	}
 	
 	/* ADMIN */
-	
+	// -- elenco chefs
 	@GetMapping("/admin/chef")
 	public String adminChefs(Model model) {
 		List<Chef> chefs = this.chefService.findAll();
 		model.addAttribute("chefs", chefs);
 		return DIR_ADMIN_PAGES_CHEF + "adminChef";
+	}
+	
+	// -- page con form per addChef
+	@GetMapping("/admin/chef/aggiungiChef")
+	public String addChef(Model model) {
+		model.addAttribute("chef", new Chef());
+		return DIR_ADMIN_PAGES_CHEF + "formChef";
+	}
+	
+	@PostMapping("/admin/chef/aggiungiChef")
+	public String aggiungiChef(@Valid @ModelAttribute("chef") Chef chef, BindingResult bindingResult,
+							   Model model) {
+		this.chefValidator.validate(chef, bindingResult);	
+		if(!bindingResult.hasErrors()) {
+			this.chefService.save(chef);
+			return this.adminChefs(model);
+		}
+		return DIR_ADMIN_PAGES_CHEF + "formChef";
 	}
 	
 	@GetMapping("/admin/chef/delete/{id}")
@@ -78,4 +101,33 @@ public class ChefController {
 		}
 		return DIR_ADMIN_PAGES_CHEF + "modificaChef";
 	}
+	
+	// -- gestione buffet per chef
+	
+	@GetMapping("/admin/chef/deleteBuffet/{idChef}/{idBuff}")
+	public String deleteBuffetFromList(@PathVariable("idChef") Long idChef,
+									   @PathVariable("idBuff") Long idBuff,
+									   Model model) {
+		Chef chef = this.chefService.findById(idChef);
+		Buffet buffet = this.buffetService.findById(idBuff);
+		chef.getBuffets().remove(buffet);
+		this.buffetService.delete(buffet);
+		this.chefService.save(chef);
+		return this.selezionaChef(idChef, model);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
